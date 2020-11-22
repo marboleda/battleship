6bf4d332eaec8b6f396cc9829140989bb57cd994
Marco Arboleda <arboleda.marco@gmail.com>
2020-11-22 15:16:14 -0500

    Move styled components outside of function components

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Grid = styled.div`
+display: grid;
+width: 30vw;
+height: 30vh;
+border-style: solid;
+border-width: 2px;
+grid-template-columns: auto auto auto auto auto auto auto auto;
+grid-template-rows: auto auto auto auto auto auto auto auto;
+`;
+
+const Cell = styled.div`
+border-style: solid;
+border-width: 0.5px;
+`;
+
+
 const grid = (props) => {
 
     const { playerType } = props;
 
-    const Grid = styled.div`
-        display: grid;
-        width: 30vw;
-        height: 30vh;
-        border-style: solid;
-        border-width: 2px;
-        grid-template-columns: auto auto auto auto auto auto auto auto;
-        grid-template-rows: auto auto auto auto auto auto auto auto;
-    `;
-
-    const Cell = styled.div`
-        border-style: solid;
-        border-width: 0.5px;
-    `;
 
     return (
         <div className = 'grid'>
