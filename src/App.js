66dc28cdc665842538951a9b980ad1a0a27dc56f
Marco Arboleda <arboleda.marco@gmail.com>
2020-11-22 15:07:50 -0500

    Visually display grids

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import Grid from './components/Grid'
+import Grid from './components/Grid';
+import styled from 'styled-components';
+
+const Game = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 90vw;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
 
 function App() {
   return (
     <div className="App">
-      <div id="game">
+      <Game id="game">
         <Grid playerType="h" />
         <Grid playerType="c" />
-      </div>
+      </Game>
     </div>
   );
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const grid = (props) => {
 
     const { playerType } = props;
 
+    const Grid = styled.div`
+        display: grid;
+        width: 30vw;
+        height: 30vh;
+        border-style: solid;
+        border-width: 2px;
+        grid-template-columns: auto auto auto auto auto auto auto auto;
+        grid-template-rows: auto auto auto auto auto auto auto auto;
+    `;
+
+    const Cell = styled.div`
+        border-style: solid;
+        border-width: 0.5px;
+    `;
+
     return (
-        <div className='grid'>
-            {[...Array(64).keys()].map((gridIndex) => {
-                return <div id={`${playerType}${gridIndex}`}></div>
-            })}
+        <div className = 'grid'>
+            <h2>{(playerType == 'h') ? 'You' : 'Enemy'}</h2>
+            <Grid>
+                {[...Array(64).keys()].map((gridIndex) => {
+                    return <Cell id={`${playerType}${gridIndex}`}></Cell>
+                })}
+            </Grid>
         </div>
     );
 };
