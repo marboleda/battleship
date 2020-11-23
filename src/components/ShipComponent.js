c48088da27c56b9ed91a76ec8dde0f10141f8cf4
Marco Arboleda <arboleda.marco@gmail.com>
2020-11-23 17:58:17 -0500

    Create divs for ships

--- src/components/ShipComponent.js
+++ src/components/ShipComponent.js
@@ -4,9 +4,38 @@ const shipComponent = (props) => {
 
     const { shipType } = props;
 
+    const createShip = (type) => {
+        let shipLength;
+        let shipSquares = [];
+
+        switch(type) {
+            case 'carrier':
+                shipLength = 5;
+                break;
+            case 'battleship':
+                shipLength = 4;
+                break;
+            case 'cruiser':
+                shipLength = 3;
+                break;
+            case 'submarine':
+                shipLength = 3;
+                break;
+            case 'destroyer':
+                shipLength = 2;
+                break;
+        }
+
+        for (let i = 0; i < shipLength; i++) {
+            shipSquares.push(<div className='ship-cell' />);
+        }
+
+        return shipSquares;
+    }
+
     return (
         <div className='shipDisplay'>
-            
+            {createShip(shipType)}
         </div>
     )
 };
