aff5a8dbfc7ccc01f79eb42367c3140f83cb3d64
Marco Arboleda <arboleda.marco@gmail.com>
2020-12-07 22:04:42 -0500

    Create first code for Game flow

---
+++ src/game/Game.js
@@ -0,0 +1,12 @@
+import Player from './Player';
+import Gameboard from './Gameboard';
+
+const game = () => {
+
+    const player = new Player("human");
+    const playerGameboard = new Gameboard();
+    const enemyGameboard = new Gameboard();
+
+}
+
+export default game;
